3cb02bfe1fdef3510bfa911bf516a5e18579a98c
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-05-22 15:19:27 +0800

    add pangram task

--- javascript/pangram.js
+++ javascript/pangram.js
@@ -1,19 +1,19 @@
 // Вам задана строка, состоящая из латинских букв, пробелов и знаков препинания.
 // Строка называется панграммой, если она содержит каждую из 26 латинских
 // букв хотя бы раз. Определите является ли строка панграммой.
-
-const letters = 'qwertyuiopasdfghjklzxcvbnm';
+// Решение должно быть O(n)
 
 function isPangram(text){
-    const sawnLetters = new Set();
+    const seen = new Set();
 
     for (const char of text.toLowerCase()){
-        if (letters.includes(char)){
-            sawnLetters.add(char);
+        if (char >= 'a' && char <= 'z'){
+            seen.add(char);
         }
+        if (seen.size === 26) return true;
     }
 
-    return sawnLetters.size === 26;
+    return false;
 }
 
 console.log(isPangram('A pangram or holoalphabetic sentence is a sentence using every letter of a given alphabet at least once.')); // => false
