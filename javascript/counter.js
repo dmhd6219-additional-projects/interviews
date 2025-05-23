567cfbddda79f9cfe5c780602a136e8b9482f9fe
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-05-23 15:58:18 +0800

    update counter task

--- javascript/counter.js
+++ javascript/counter.js
@@ -5,22 +5,41 @@
  */
 
 var createCounter = function (init) {
- return {
-  value: init,
+    return {
+        init: init,
+        value: init,
 
-  increment: function () {
-   return ++this.value;
-  },
+        increment: function () {
+            return ++this.value;
+        },
 
-  decrement: function () {
-   return --this.value;
-  },
+        decrement: function () {
+            return --this.value;
+        },
 
-  reset: function () {
-   this.value = init;
-   return this.value;
-  }
- };
+        reset: function () {
+            this.value = this.init;
+            return this.value;
+        }
+    };
+};
+
+var createCounterWithClosure = function (init) {
+    let currentValue = init;
+    return {
+        increment: function () {
+            return ++currentValue;
+        },
+
+        decrement: function () {
+            return --currentValue;
+        },
+
+        reset: function () {
+            currentValue = init;
+            return currentValue;
+        }
+    };
 };
 
 
@@ -28,3 +47,8 @@ const counter = createCounter(5)
 console.log(counter.increment()); // 6
 console.log(counter.reset()); // 5
 console.log(counter.decrement()); // 4
+
+const counterWithClosure = createCounterWithClosure(5)
+console.log(counterWithClosure.increment()); // 6
+console.log(counterWithClosure.reset()); // 5
+console.log(counterWithClosure.decrement()); // 4
