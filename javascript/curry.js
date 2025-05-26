25bba856508a42a40eba367cd8cc3e0812a8813a
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-05-26 15:07:41 +0800

    update curry task

--- javascript/curry.js
+++ javascript/curry.js
@@ -14,6 +14,19 @@ function curry(fn) {
     return f;
 }
 
+function curry2(fn) {
+    return function inner(...args) {
+        if (args.length >= fn.length){
+            return fn(...args)
+        }
+        return (...newArgs) => inner(...args, ...newArgs)
+    }
+}
+
 console.log(curry(sum)(1, 2, 3, 4, 5, 6)); // 6
 console.log(curry(sum)(1, 2)(3)); // 6 const a = curry(sum)(1, 2); a(3) === 6
 console.log(curry(sum)(1)(2)(3)); // 6
+
+console.log(curry2(sum)(1, 2, 3, 4, 5, 6)); // 6
+console.log(curry2(sum)(1, 2)(3)); // 6 const a = curry(sum)(1, 2); a(3) === 6
+console.log(curry2(sum)(1)(2)(3)); // 6
